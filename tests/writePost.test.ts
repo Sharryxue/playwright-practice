3d6c85c0f036b24c586cccb1f2f2d18eee1aa361
Sharry Shi <sharry.shi@outlook.com>
2024-08-02 19:22:02 +0800

    Add a test to post article

--- tests/writePost.test.ts
+++ tests/writePost.test.ts
@@ -59,3 +59,58 @@ test("Can post page", async ({page}) => {
     await expect(successMessage).toHaveText('页面已经被删除');
     expect(tableCheckbox).not.toBeVisible();
 });
+
+test("Can post article", async ({page}) => {
+    //Setup Action
+    // See if this can make the login test reuseble
+    //await login.loginTest();
+
+    //Navigate to the URL
+    await page.goto(`http://alanzh.com/`);
+
+    //Locators
+    const loginButton = page.locator(`.submit`);
+    const loginLink = page.getByText(`登录`);
+    const userName = page.getByPlaceholder(`用户名`);
+    const userPassword = page.getByPlaceholder(`密码`);
+
+    //Actions
+    await loginLink.click();
+    await userName.fill('test');
+    await userPassword.fill('000000');
+    await loginButton.click();
+
+    //Locators
+    /*  find the way to make the locator be reuseble, as the only difference for buttons in navigator is the text.
+    see if there is a way to make the text be a variable. */
+    const articleSummary = page.getByPlaceholder(`标题`);
+    const articleContent = page.locator(`textarea#text`);
+    const articlePostButton = page.getByText(`发布文章`);
+   // This is due to issue found in https://sharry-shi.atlassian.net/browse/SCRUM-27?focusedCommentId=10010
+    const oprateDropdown = page.locator(`(//div[contains(@class,'row')]//div[@class='operate']//button)[1]`);
+    const postArticleLink = page.getByText('撰写文章');
+    const postLink = page.locator("nav .parent a[href*='write']");
+    const removeOption = page.locator(`(//div[contains(@class,'row')]//div[@class='operate']//a)[1]`);
+    const successMessage = page.locator(`.success`);
+    const tableCheckbox = page.locator(`//tr[contains(.,'测试文章标题')]//input[(@type='checkbox')]`);
+
+    //Actions
+    await postLink.hover();
+    await postArticleLink.click();
+    await articleSummary.fill('测试文章标题');
+    await articleContent.fill('这是一个测试文章, 测试结束时将被删除, 请谨慎使用.');
+    await articlePostButton.click();
+
+    //Assertion
+    await expect(successMessage).toHaveText('文章 "测试文章标题" 已经发布');
+    expect(tableCheckbox).toBeVisible();
+
+    
+    //Teardown Action
+    await tableCheckbox.check();
+    await oprateDropdown.click();
+    page.on('dialog', dialog => dialog.accept ());
+    await removeOption.click();
+    await expect(successMessage).toHaveText('文章已经被删除');
+    expect(tableCheckbox).not.toBeVisible();
+});
